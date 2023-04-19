675a36fc9ee6d53e2fe9b2547c96e2b1fdb35c36
swayangjit <swayangjit029@gmail.com>
2023-04-19 11:16:27 +0530

    Issue #IA-000 chore: Inital commit

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# ionic-6-angular-13
+# Ionic 6 with Angular 13
+
+## Setup
+
+Requirements to use this project:
+
+##### Node.js (https://nodejs.org/download/)
+
+##### npm (Node Package Manager, it comes with node.js installation)
+In case you're not with the latest version of npm:
+```sh
+$ sudo npm install npm -g
+```
+
+##### Cordova & Ionic Cli
+To install both of them on your system just launch this command:
+```sh
+$ sudo npm install cordova ionic -g
+```
+
+## Install NPM Dependencies
+Once you clone this repository, run this command on your terminal to install all needed dependencies:
+```sh
+$ npm install
+
+our
+
+$ npm install --force
+
+```
+
+## Install cordova plugin Dependencies
+Run this command on your terminal to add a platform and install all needed puglins:
+
+iOS:
+```sh
+$ ionic cordova platform add ios
+$ ionic cordova run ios
+```
+
+Android:
+```sh
+$ ionic cordova platform add android
+$ ionic cordova run android
+```
+## Launching the App
+After installing the needed dependencies you are done, launch your app with a simple
+```sh
+$ ionic serve
+```
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+
+import { Platform, MenuController } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    }
+
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    public menuCtrl: MenuController
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  darkTheme(event) {
+    if (event.detail.checked) {
+      // Use matchMedia to check the user preference
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+      toggleDarkTheme(prefersDark.matches);
+      // Listen for changes to the prefers-color-scheme media query
+      prefersDark.addListener((mediaQuery) => toggleDarkTheme(mediaQuery.matches));
+      // Add or remove the "dark" class based on if the media query matches
+      function toggleDarkTheme(shouldAdd) {
+        document.body.classList.toggle('dark', shouldAdd);
+      }
+
+    } else {
+      // Use matchMedia to check the user preference
+      const prefersDark = window.matchMedia('(prefers-color-scheme: :root)');
+      toggleDarkTheme(prefersDark.matches);
+      // Listen for changes to the prefers-color-scheme media query
+      prefersDark.addListener((mediaQuery) => toggleDarkTheme(mediaQuery.matches));
+      // Add or remove the "dark" class based on if the media query matches
+      function toggleDarkTheme(shouldAdd) {
+        document.body.classList.toggle('dark', shouldAdd);
+      }
+    }
+
+  }
+
+}
